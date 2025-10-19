d95fab1372c51948d291f976d9fbff1d2a7e720d
glimmer <glimmerddl@gmail.com>
2025-10-19 15:01:09 +0100

    chinese version

---
+++ examples/4_1d_sin_move_left.py
@@ -0,0 +1,23 @@
+import numpy as np
+from wave_simulator import OneDimensionSimulator
+
+
+def my_initial_wave(x):
+    if x > 2 and x < np.pi/4+2:
+        return np.sin(8*(x-2))
+    return 0
+
+
+def my_initial_speed(x):
+    if x > 2 and x < np.pi/4+2:
+        return 8*np.cos(8*(x-2))
+    return 0
+
+
+s = OneDimensionSimulator()
+s.set_space_range(np.pi, 0.01)
+s.set_initial_wave(my_initial_wave)
+s.set_initial_point_speed(my_initial_speed)
+s.simulate()
+s.animate_result_1D(ylim=(-4, 4), down_sampling_rate=20,
+                    save_path="/Users/glimmer/Documents/study/mathematics_of_light_and_sound/wave-simulation/output/8.mp4")
--- src/wave_simulator/oned_simulator.py
+++ src/wave_simulator/oned_simulator.py
@@ -1,65 +1,14 @@
 import numpy as np
-import abc
-from typing import Callable, Any
-
-
-class BoundaryCondition(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def apply(self, A_0: np.float64, A_1: np.float64, **kwargs: Any) -> np.float64:
-        """
-        以左边界为例，输入上一时刻的 A_0, A_1，输出下一时刻的 A_0
-        """
-        raise RuntimeError("not implemented")
-
-
-class FixedBoundary(BoundaryCondition):
-    """
-    固定边界
-    """
-
-    def __init__(self, value=np.float64(0.0)):
-        super().__init__()
-        self.value = value
-
-    def apply(self, A_0: np.float64, A_1: np.float64, **kwargs: Any) -> np.float64:
-        return self.value
-
-
-class NeumannBoundary(BoundaryCondition):
-    """
-    边界点的位移梯度（斜率）为零（例如，一根绳子的末端系在一个可以在杆上自由滑动的环上）。
-    """
-
-    def apply(self, A_0: np.float64, A_1: np.float64, **kwargs: any) -> np.float64:
-        # C=c*dt/dx
-        C = kwargs.get("C")
-        if C is None:
-            raise ValueError("C is not set")
-        A_0_last = kwargs.get("A_0_last")
-        if A_0_last is None:
-            raise ValueError("A_0_last is not set")
-
-        return 2*A_0 - A_0_last + C**2*(A_1-A_0)
-
-
-class UnlimitedBoundary(BoundaryCondition):
-    """
-    无限制边界
-    """
-
-    def apply(self, u_0: np.float64, u_1: np.float64, **kwargs: any) -> np.float64:
-        C = kwargs.get("C")
-        if C is None:
-            raise ValueError("C is not set")
-
-        return (1-C)*u_0+C*u_1
+from typing import Callable
+from wave_simulator.boundary_conditions import *
+from wave_simulator.animation_utils import animate_result_1D
 
 
 class OneDimensionSimulator:
     def __init__(self):
         # 空间
         self.L_x = np.pi/2  # 仿真的距离范围，从 0 到 L_x
-        self.dx = 0.01  # 仿真的最小距离间隔
+        self.dx = 0.001     # 仿真的最小距离间隔
         self.N = int(self.L_x/self.dx)  # 仿真的线段的个数
         self.X = np.linspace(0, self.L_x, self.N+1,
                              dtype=np.float64)  # 仿真的空间范围
@@ -70,8 +19,8 @@ class OneDimensionSimulator:
         self.T = np.linspace(0, self.L_t, self.N_t+1)  # 时间范围
 
         # 边界条件
-        self.left_boundary = FixedBoundary()
-        self.right_boundary = FixedBoundary()
+        self.left_boundary = UnlimitedBoundary()
+        self.right_boundary = UnlimitedBoundary()
 
         # 初始波形，默认全为 0
         self.initial_wave = lambda x: 0.0
@@ -79,6 +28,17 @@ class OneDimensionSimulator:
         self.initial_point_speed = lambda x: 0.0
         # 介质波速，默认为 1
         self.wave_speed = lambda x: 1.0
+
+    def set_simulation_range(self, L_x, dx, L_t, dt):
+        self.L_x = L_x
+        self.dx = dx
+        self.N = int(self.L_x/self.dx)
+        self.X = np.linspace(0, self.L_x, self.N+1,
+                             dtype=np.float64)
+        self.L_t = L_t
+        self.dt = dt
+        self.N_t = int(self.L_t/self.dt)
+        self.T = np.linspace(0, self.L_t, self.N_t+1)
 
     def set_space_range(self, L_x, dx):
         self.L_x = L_x  # 仿真的距离范围，从 0 到 L_x
@@ -127,6 +87,10 @@ class OneDimensionSimulator:
     def set_right_boundary(self, boundary: BoundaryCondition):
         self.right_boundary = boundary
 
+    def set_all_boundary(self, boundary: BoundaryCondition):
+        self.left_boundary = boundary
+        self.right_boundary = boundary
+
     def simulate(self):
         """
         开始仿真
@@ -135,22 +99,26 @@ class OneDimensionSimulator:
         self.result = np.zeros((self.N+1, self.N_t+1), np.float64)
 
         # 用于储存当前 i-1,i,i+1 时刻的波形
-        A_last = np.zeros(self.N+1, np.float64)
-        A_current = np.zeros(self.N+1, np.float64)
-        A_next = np.zeros(self.N+1, np.float64)
+        u_last = np.zeros(self.N+1, np.float64)
+        u_current = np.zeros(self.N+1, np.float64)
+        u_next = np.zeros(self.N+1, np.float64)
         # 介质波速
         c = np.zeros(self.N+1, np.float64)
         # 质点初始速度
         initial_v = np.zeros(self.N+1, np.float64)
         for i in range(0, self.N+1):
             c[i] = self.wave_speed(self.X[i])
-            A_last[i] = self.initial_wave(self.X[i])
+            u_last[i] = self.initial_wave(self.X[i])
             initial_v[i] = self.initial_point_speed(self.X[i])
-        # c2 为 c 的平方
-        c2 = c**2
 
-        # 仿真需要用 A_last 和 A_current 递推 A_next，所以我们从 t=1 开始仿真
-        # 用 t=0 和 t=1 的初始条件，填充 A_last 和 A_current
+        # check CFL
+        C = c*self.dt/self.dx
+        if np.any(C > 0.1):
+            raise ValueError(
+                "CFL check failed, you should reduce dt, wave speed or increase dx")
+
+        # 仿真需要用 u_last 和 u_current 递推 u_next，所以我们从 t=1 开始仿真
+        # 用 t=0 和 t=1 的初始条件，填充 u_last 和 u_current
 
         initial_a = np.zeros(self.N+1, np.float64)
         # 为了执行效率这里不直接遍历所有的n，而是直接对numpy的array进行操作。可读性较差但没办法，python就是这么垃圾。
@@ -160,83 +128,68 @@ class OneDimensionSimulator:
         # 表示 i+1  位置的list的索引为 2,...,N    用切片表示就是 [2:N+1]
         # 理解不了就想想对于同一个索引 i ，在不同的list里的数据是什么
 
-        # (c_i+1)^2
+        c2 = c**2
         c2_i_sub_1 = c2[0:self.N-1]
         c2_i = c2[1:self.N]
         c2_i_add_1 = c2[2:self.N+1]
 
-        A_i_sub_1 = A_last[0:self.N-1]
-        A_i = A_last[1:self.N]
-        A_i_add_1 = A_last[2:self.N+1]
+        u_i_sub_1 = u_last[0:self.N-1]
+        u_i = u_last[1:self.N]
+        u_i_add_1 = u_last[2:self.N+1]
         # 计算初始加速度
         initial_a = np.zeros(self.N+1, np.float64)
         initial_a[1:self.N] = 1/self.dx**2 * (
-            0.5*(c2_i_add_1 + c2_i)*(A_i_add_1-A_i)
-            - 0.5*(c2_i+c2_i_sub_1)*(A_i-A_i_sub_1))
+            0.5*(c2_i_add_1 + c2_i)*(u_i_add_1-u_i)
+            - 0.5*(c2_i+c2_i_sub_1)*(u_i-u_i_sub_1))
         # 计算 t=1 时刻的波形
-        A_current[1:self.N] = A_last[1:self.N] + \
+        u_current[1:self.N] = u_last[1:self.N] + \
             initial_v[1:self.N] * self.dt +  \
             0.5 * initial_a[1:self.N] * self.dt**2
         # 应用边界条件
-        A_current[0] = self.left_boundary.apply(
-            A_last[0], A_last[1],
+        u_current[0] = self.left_boundary.apply(
+            u_last[0], u_last[1],
             C=c[0]*self.dt/self.dx,
-            A_0_last=A_last[0] - initial_v[0] * self.dt,
+            u_0_last=u_last[0] - initial_v[0] * self.dt,
         )
-        A_current[self.N] = self.right_boundary.apply(
-            A_last[self.N], A_last[self.N-1],
+        u_current[self.N] = self.right_boundary.apply(
+            u_last[self.N], u_last[self.N-1],
             C=c[self.N]*self.dt/self.dx,
-            A_0_last=A_last[self.N] - initial_v[self.N] * self.dt,
+            u_0_last=u_last[self.N] - initial_v[self.N] * self.dt,
         )
 
-        self.result[:, 0] = A_last.copy()
-        self.result[:, 1] = A_current.copy()
+        self.result[:, 0] = u_last.copy()
+        self.result[:, 1] = u_current.copy()
         for i in range(1, self.N_t):
             # 计算非边界的点 1,...,N-1
             # 表示 i-1  位置的list的索引为 0,...,N-2  用切片表示就是 [0:N-1]
             # 那么表示 i 位置的list的索引为 1,...,N-1  用切片表示就是 [1:N]
             # 表示 i+1  位置的list的索引为 2,...,N    用切片表示就是 [2:N+1]
-            A_current_i_sub_1 = A_current[0:self.N-1]
-            A_current_i = A_current[1:self.N]
-            A_current_i_add_1 = A_current[2:self.N+1]
-            A_last_i = A_last[1:self.N]
+            u_current_i_sub_1 = u_current[0:self.N-1]
+            u_current_i = u_current[1:self.N]
+            u_current_i_add_1 = u_current[2:self.N+1]
+            u_last_i = u_last[1:self.N]
 
-            A_next_i = 2*A_current_i - A_last_i + (self.dt/self.dx)**2*(
-                0.5*(c2_i_add_1+c2_i)*(A_current_i_add_1-A_current_i)
-                - 0.5*(c2_i + c2_i_sub_1)*(A_current_i - A_current_i_sub_1)
+            u_next_i = 2*u_current_i - u_last_i + (self.dt/self.dx)**2*(
+                0.5*(c2_i_add_1+c2_i)*(u_current_i_add_1-u_current_i)
+                - 0.5*(c2_i + c2_i_sub_1)*(u_current_i - u_current_i_sub_1)
             )
-            A_next[1:self.N] = A_next_i
+            u_next[1:self.N] = u_next_i
             # 计算边界的点
-            A_next[0] = self.left_boundary.apply(
-                A_current[0], A_current[1],
+            u_next[0] = self.left_boundary.apply(
+                u_current[0], u_current[1],
                 C=c[0]*self.dt/self.dx,
-                A_0_last=A_last[0],
+                u_0_last=u_last[0],
             )
-            A_next[self.N] = self.right_boundary.apply(
-                A_current[self.N], A_current[self.N-1],
+            u_next[self.N] = self.right_boundary.apply(
+                u_current[self.N], u_current[self.N-1],
                 C=c[self.N]*self.dt/self.dx,
-                A_0_last=A_last[self.N],
+                u_0_last=u_last[self.N],
             )
 
-            self.result[:, i+1] = A_next.copy()
-            A_last[:] = A_current.copy()
-            A_current[:] = A_next.copy()
+            self.result[:, i+1] = u_next.copy()
+            u_last[:] = u_current.copy()
+            u_current[:] = u_next.copy()
 
-
-if __name__ == "__main__":
-    s = OneDimensionSimulator()
-    # s.set_space_range(3, 0.01)
-    # s.set_initial_wave(lambda x: np.where(x < 2, x, 6-2*x))
-    s.set_initial_wave(lambda x: np.where(x < np.pi/4, 3*np.sin(8*x), 0))
-    s.set_initial_point_speed(
-        lambda x: np.where(x < np.pi/4, -24*np.cos(8*x), 0))
-
-    s.set_left_boundary(UnlimitedBoundary())
-    s.set_right_boundary(UnlimitedBoundary())
-    s.simulate()
-    print(s.result)
-    import matplotlib.pyplot as plt
-    import viz_tools  # self-developed module that groups animation functions
-    anim1 = viz_tools.anim_1D(s.X, s.result, s.dt, 10, save=False,
-                              myxlim=(0, s.L_x))
-    plt.show()
+    def animate_result_1D(self, **args):
+        animate_result_1D(self.result, self.X, self.dt,
+                          xlim=(0, self.L_x), **args)
